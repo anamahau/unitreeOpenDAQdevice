14e7707e6392e3b5f7c6a673f9c131167db8e67b
Tomaz Cvetko <tomaz.cvetko@opendaq.com>
2025-12-16 08:03:51 +0100

    Remove example_module

--- example_module/src/module_dll.cpp
+++
@@ -1,9 +0,0 @@
-#include <example_module/module_dll.h>
-#include <example_module/example_module.h>
-
-#include <opendaq/module_factory.h>
-
-using namespace daq::modules::example_module;
-
-DEFINE_MODULE_EXPORTS(ExampleModule)
-
